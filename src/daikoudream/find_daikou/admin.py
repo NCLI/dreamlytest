3a4f4ad6930692866f7b95733757672d2d2eafda
Nicholas Ipsen <ncli@ncli-design.com>
2023-02-16 15:06:04 +0100

    Added models to admin site.

--- src/daikoudream/find_daikou/admin.py
+++ src/daikoudream/find_daikou/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import CustomUser, Customer, Car, Driver, Order
 
-# Register your models here.
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_driver', 'is_customer')
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('user', 'saved_payment_info')
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('make', 'model', 'year', 'customer')
+
+class DriverAdmin(admin.ModelAdmin):
+    list_display = ('user', 'is_active', 'latitude', 'longitude', 'license_number', 'bank_account_info')
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'driver', 'car', 'pickup_time', 'completed')
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Car, CarAdmin)
+admin.site.register(Driver, DriverAdmin)
+admin.site.register(Order, OrderAdmin)
