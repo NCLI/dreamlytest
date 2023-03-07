f4fabd44dc1953e9cfaa8b7f51a688973f1558f4
Nicholas Ipsen <ncli@ncli-design.com>
2023-03-07 22:32:33 +0100

    Improved url structure.

--- src/daikoudream/daikoudream/urls.py
+++ src/daikoudream/daikoudream/urls.py
@@ -18,11 +18,24 @@ from django.urls import path, include
 
 from django.contrib.auth.views import LoginView, LogoutView
 
-from find_daikou.views import register
+import find_daikou.views as views
 
 urlpatterns = [
-    path('dashboard/', include('find_daikou.urls')),
-    path('register/', register, name='register'),
+    path('', views.index, name='index'),
+    path('test/', views.available_drivers, name='driverlist'),
+    path('confirm_order', views.confirm_order, name='confirm_order'),
+    path('call_driver/', views.call_driver, name='call_driver'),
+    # path('modify_driver/', views.modify_driver, name='modify_driver'),
+    path('modify_car/', views.modify_car, name='modify_car'),
+    path('add_car/', views.add_car, name='add_car'),
+    path('modify_user/', views.modify_user, name='modify_user'),
+    path('history/', views.history, name='history'),
+    # path('open_orders', views.open_orders, name='open_orders'),
+    path('set_driver_available/', views.set_driver_available, name='set_driver_available'),
+    path('set_driver_unavailable/', views.set_driver_unavailable, name='set_driver_unavailable'),
+    path('cancel_order/', views.cancel_order, name='cancel_order'),
+    path('update_eta/', views.update_eta, name='update_eta'),
+    path('register/', views.register, name='register'),
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
--- src/daikoudream/find_daikou/urls.py
+++ src/daikoudream/find_daikou/urls.py
@@ -3,18 +3,4 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('test/', views.available_drivers, name='driverlist'),
-    path('confirm_order', views.confirm_order, name='confirm_order'),
-    path('call_driver/', views.call_driver, name='call_driver'),
-    # path('modify_driver/', views.modify_driver, name='modify_driver'),
-    path('modify_car/', views.modify_car, name='modify_car'),
-    path('add_car/', views.add_car, name='add_car'),
-    path('modify_user/', views.modify_user, name='modify_user'),
-    path('history/', views.history, name='history'),
-    # path('open_orders', views.open_orders, name='open_orders'),
-    path('set_driver_available/', views.set_driver_available, name='set_driver_available'),
-    path('set_driver_unavailable/', views.set_driver_unavailable, name='set_driver_unavailable'),
-    path('cancel_order/', views.cancel_order, name='cancel_order'),
-    path('update_eta/', views.update_eta, name='update_eta'),
 ]
